da051afaf2653f85b605efb66b1e232518c5eb87
AmirN1234 <s11850583@students.duvalschools.org>
2024-09-12 12:56:53 -0400

    awards.py

---
+++ .github/awarding extralive.py
@@ -0,0 +1,15 @@
+# Awarding Extra Lives, Amir Norflett, v0.0
+
+
+lives = 3
+
+# Allow the user to input the score AS AN INTEGER
+
+# If score is 1000 or less
+    # lose a life 
+# If score is > 10000 but less than 10001
+    # Give 1 Extra Life 
+# If score is > 100000 
+# Give 2 Extra Lives
+
+# Output the score and number of lives to the screen 
