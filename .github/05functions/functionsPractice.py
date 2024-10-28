5d9785829b1ae90d04f4c54971e7ab4f71d81b2a
AmirN1234 <s11850583@students.duvalschools.org>
2024-10-28 13:14:20 -0400

    functionpyyyyyyy
    tyxrtc v

--- .github/05functions/functionsPractice.py
+++ .github/05functions/functionsPractice.py
@@ -19,24 +19,42 @@ def helloWorldMulti(): # FUNCTION SIGNATURE
     language = input("What language do you want?\n Please type the first letter of the language you want.\n").lower()
 
     # print "Hello, World!" to the screen in that language 
-    print("The languages avaliable English, Spanish, French.\n")
+
+    if language == "e":
+        print("In English:\nHello,World!")
+    elif language == "s":
+        print("In Spanish:\nHola, Mundo!\n")
+    elif language == "f":
+        print("In French:\nBon jour, Le Monde!\n")
+    elif language == "g":
+        print("In German:\n, hallo, Walt!\n")
+    else:
+        print("In Somali:\n, Haye caalamkow!\n")
+
 helloWorldMulti()
 
+# Function to Determine Even / Odd Numbers 
+arugment1 = random.randint(-1000, 1000)
 
+def isEven(argument1: int) -> bool: # Requires one ARGUMENT (argument1) and RETURNS a Boolean value.
+    """Determines if an integer value is even or odd"""
+    if argument1 % 2 == 0:
+        return True 
+    else:
+        return False
 
+print(isEven(argument1)) 
 
+# Function with Multiple Arguments
+def canRideRollerCoaster(age: int, height: int) -> bool:
+    if age > 10 and height > 4:
+        print("You can ride.\n")
+        return True 
+    else:
+        print("You cannot ride.\n")
+        return False 
 
-
-
-
-
-
-
-
-
-
-
-
+canRideRollerCoaster(4, 10) # Arguments must be passed in the same order as the function signature indicates.
 
 
 
